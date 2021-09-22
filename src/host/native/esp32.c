df4ddebaf86ae10a07ff540854ce5b3642a30f2b
Vasyl Havryliak <gavrilyak@gmail.com>
2021-09-22 10:50:56 +0300

    get mac from fuse to avoid initing wifi

--- src/host/native/esp32.c
+++ src/host/native/esp32.c
@@ -1,11 +1,13 @@
 #include "xsmc.h"
 #include "xsHost.h"
-#include <esp_wifi.h>
+//#include <esp_wifi.h>
+#include "esp_system.h"
 
 void xs_getmac(xsMachine *the)
 {
   xsmcSetArrayBuffer(xsResult, NULL, 6);
-  esp_wifi_get_mac(xsmcToInteger(xsArgc) > 0? WIFI_IF_AP : WIFI_IF_STA, xsmcToArrayBuffer(xsResult));
+  //esp_wifi_get_mac(xsmcToInteger(xsArgc) > 0? WIFI_IF_AP : WIFI_IF_STA, xsmcToArrayBuffer(xsResult));
+  esp_efuse_mac_get_default(xsmcToArrayBuffer(xsResult));
 }
 
 void xs_do_restart(xsMachine *the)
