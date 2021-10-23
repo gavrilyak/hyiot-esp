5a7651200fb06862dff19fb7980764f568a8ba79
Vasyl Havryliak <gavrilyak@gmail.com>
2021-10-23 19:15:17 +0300

    new default device id calculation - 35 bits of MAC

--- src/lib/getDefaultDeviceId.js
+++ src/lib/getDefaultDeviceId.js
@@ -1,5 +1,25 @@
-import { getMAC } from "native/esp32";
+import { getMacBin } from "native/esp32";
 
+var ALPHABET = "0123456789abcdefghjkmnpqrstvwxyz"; //crock
+function int30toBase32(n, abet = ALPHABET) {
+  let result =
+    abet[(n >> 25) & 0x1f] +
+    abet[(n >> 20) & 0x1f] +
+    abet[(n >> 15) & 0x1f] +
+    abet[(n >> 10) & 0x1f] +
+    abet[(n >> 5) & 0x1f] +
+    abet[(n >> 0) & 0x1f];
+  return result;
+}
+
+function calcDefaultDeviceId(mac) {
+  const mac32 = new DataView(mac.buffer).getUint32(mac.length - 4);
+  const mac30 = mac32 >>> 2; // two last bits are alway zero in esp32, it has 4 MACS
+  const id = ALPHABET[mac[1] & 0x1f] + int30toBase32(mac30);
+  return id;
+}
+
+const calculatedId = calcDefaultDeviceId(new Uint8Array(getMacBin()));
 export default function getDefaultDeviceId() {
-  return "HL1-" + getMAC().slice(-8).replaceAll(":", "");
+  return calculatedId;
 }
